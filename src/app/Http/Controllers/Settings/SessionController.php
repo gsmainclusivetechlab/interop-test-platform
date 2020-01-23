d5c5efa69a8cb42fa513cc3f851844c749a93dc9
akokorev <akokorev@justcoded.co>
2020-01-23 11:50:41 +0200

    Update views

---
+++ src/app/Http/Controllers/Settings/SessionController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Settings;
+
+use App\Models\TestSession;
+use App\Http\Controllers\Controller;
+
+class SessionController extends Controller
+{
+    /**
+     * UserController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified']);
+        $this->authorizeResource(TestSession::class, 'session');
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        $sessions = TestSession::latest()->paginate();
+
+        return view('settings.sessions.index', compact('sessions'));
+    }
+}
