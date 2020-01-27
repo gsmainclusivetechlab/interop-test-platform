59a5bb0d276c63e5e08294b92736e2610a4c014d
akokorev <akokorev@justcoded.co>
2020-01-27 12:45:34 +0200

    Hotfix

--- src/resources/views/layouts/account.blade.php
+++ src/resources/views/layouts/account.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('sidebar')
-    <h1 class="page-title mb-5">{{ __('Account') }}</h1>
+    <h1 class="page-title mb-5">
+        {{ __('Account') }}
+    </h1>
     <div>
         <div class="list-group list-group-transparent mb-0">
             <a href="{{ route('account.profile.edit') }}" class="list-group-item list-group-item-action d-flex align-items-center @if (request()->routeIs('account.profile.edit')) active @endif">
