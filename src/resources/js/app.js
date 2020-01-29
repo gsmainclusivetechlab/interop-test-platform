3bf64e16d8bec8bafcd9396c592003012f1fb4ec
Александр Кокорев <akokorev@justcoded.co>
2020-01-29 21:50:53 +0200

    Adding diagram component

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,10 +1,10 @@
 require('./bootstrap');
 
-const confirmPopup = require('./modules/confirmPopup');
-const flowChart = require('./modules/flowChart');
-
-confirmPopup.init();
-flowChart.init();
+// const confirmPopup = require('./modules/confirmPopup');
+// const flowChart = require('./modules/flowChart');
+//
+// confirmPopup.init();
+// flowChart.init();
 
 $('body').tooltip({ selector: '[data-toggle=tooltip]' });
 
@@ -21,7 +21,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('diagram-component', require('./components/DiagramComponent.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
---
+++ src/resources/js/components/SelectComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    // import mermaid from 'mermaid';
+
+    export default {
+        mounted() {
+            console.log(this.$el);
+        }
+    }
+</script>
