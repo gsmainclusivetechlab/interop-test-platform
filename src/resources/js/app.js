869bd24619099677cd7cf486d94f3f229f55b112
jc_ohladkov <ohladkov@justcoded.co>
2020-02-03 13:51:44 +0200

    add chart component

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap');
 
 Vue.use(BootstrapVue);
 Vue.component('confirm-button', require('./components/ConfirmButton.vue').default);
+Vue.component('chart', require('./components/Chart.vue').default);
 
 const app = new Vue({
     el: '#app',
