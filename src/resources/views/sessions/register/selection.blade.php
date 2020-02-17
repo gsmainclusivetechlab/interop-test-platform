e7d228c98f0c12f56a6f97be6892607853bd91dd
jc_ohladkov <ohladkov@justcoded.co>
2020-02-17 13:39:38 +0200

    update session creation page layout

--- src/resources/views/sessions/register/selection.blade.php
+++ src/resources/views/sessions/register/selection.blade.php
@@ -13,31 +13,37 @@
                         @include('sessions.register.includes.components')
                     </div>
                     <div class="d-flex justify-content-between">
-                        @include('sessions.register.includes.components-info')
-                        <div class="flow-chart-content">
-                            <div class="card">
-                                <div class="card-header border-0">
-                                    <h3 class="card-title">@yield('title')</h3>
-                                </div>
-                                <div class="card-body">
-                                    <div class="form-group">
-                                        <label class="form-label">
-                                            {{ __('SUT') }}
-                                        </label>
-                                        <select class="form-control custom-select">
-                                            <option value="sp">{{ __('Service Provider') }}</option>
-                                            <option value="mmo-1" disabled>{{ __('Mobile Money Operator 1') }}</option>
-                                            <option value="mojaloop" disabled>{{ __('Mojaloop') }}</option>
-                                            <option value="mmo-2" disabled>{{ __('Mobily Money Operator 2') }}</option>
-                                        </select>
-                                        <small class="form-text text-muted">
-                                            {{ __('Now only Service Provider can be set as System Under Test.') }}
-                                        </small>
+                        <div class="d-flex w-100">
+                            <div class="col-4">
+                            @include('sessions.register.includes.components-info')
+                            </div>
+                            <div class="col-8">
+                                <div class="flow-chart-content">
+                                    <div class="card">
+                                        <div class="card-header border-0">
+                                            <h3 class="card-title">@yield('title')</h3>
+                                        </div>
+                                        <div class="card-body">
+                                            <div class="form-group">
+                                                <label class="form-label">
+                                                    {{ __('SUT') }}
+                                                </label>
+                                                <select class="form-control custom-select">
+                                                    <option value="sp">{{ __('Service Provider') }}</option>
+                                                    <option value="mmo-1" disabled>{{ __('Mobile Money Operator 1') }}</option>
+                                                    <option value="mojaloop" disabled>{{ __('Mojaloop') }}</option>
+                                                    <option value="mmo-2" disabled>{{ __('Mobily Money Operator 2') }}</option>
+                                                </select>
+                                                <small class="form-text text-muted">
+                                                    {{ __('Now only Service Provider can be set as System Under Test.') }}
+                                                </small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-end">
+                                        <button type="submit" class="btn btn-primary">{{ __('Next') }}</button>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="d-flex justify-content-end">
-                                <button type="submit" class="btn btn-primary">{{ __('Next') }}</button>
                             </div>
                         </div>
                     </div>
