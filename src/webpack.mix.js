ddbd88eabbdaf29bb5f786dc968d5e27a840251e
akokorev <akokorev@justcoded.co>
2020-02-17 11:30:11 +0200

    Working with webpack assets build

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -3,17 +3,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
+mix.setPublicPath('public/assets');
 mix.babelConfig({
     plugins: ['@babel/plugin-syntax-dynamic-import'],
 })
@@ -23,17 +13,15 @@ mix.babelConfig({
         },
         plugins: [
             new CleanWebpackPlugin({
-                cleanOnceBeforeBuildPatterns: [
-                    path.resolve(process.cwd(), 'public/js'),
-                ],
+                cleanOnceBeforeBuildPatterns: ['public/assets/**/*', '!.gitignore'],
             }),
             new BundleAnalyzerPlugin({
                 analyzerMode: 'static',
-                reportFilename: './webpack-stats/report.html',
+                reportFilename: 'webpack-stats.html',
             }),
         ],
     })
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/vendor.scss', 'public/css')
-    .sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/app.js', 'js')
+    .sass('resources/sass/vendor.scss', 'css')
+    .sass('resources/sass/app.scss', 'css')
     .version();
