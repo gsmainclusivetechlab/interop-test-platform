0a7e7e1bb486b7021a3b6301dea86f57ea5b7d9c
Александр Кокорев <akokorev@justcoded.co>
2020-03-24 14:27:33 +0200

    Hotfix

--- src/resources/views/sessions/includes/test-data-example.blade.php
+++ src/resources/views/sessions/includes/test-data-example.blade.php
@@ -28,7 +28,7 @@
                 </div>
                 <div class="w-75 px-4 py-2 border">
                     <div class="mb-0 p-0 bg-transparent">
-                        <pre class="mb-0 p-0"><code v-pre>@json($request->headers(), JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES)</code></pre>
+                        <pre class="mb-0 p-0"><code v-pre>@json($request->headerLines(), JSON_PRETTY_PRINT|JSON_UNESCAPED_SLASHES)</code></pre>
                     </div>
                 </div>
             </div>
