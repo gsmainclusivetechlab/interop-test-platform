6a4d5ed71565ab191d3ce58b20643a7c8f29d07a
jc_ohladkov <ohladkov@justcoded.co>
2020-04-08 18:14:30 +0300

    add styles for test-steps table

--- src/resources/views/sessions/test-steps/index.blade.php
+++ src/resources/views/sessions/test-steps/index.blade.php
@@ -3,63 +3,74 @@
 @section('title', $session->name)
 
 @section('content')
-	<div class="table-responsive mb-0">
-		<table class="table table-striped table-hover card-table">
-			<thead class="thead-light">
-			<tr>
-				<th class="text-nowrap w-auto">{{ __('Step') }}</th>
-				<th class="text-nowrap w-auto">{{ __('Source') }}</th>
-				<th class="text-nowrap w-auto">{{ __('Target') }}</th>
-				<th class="text-nowrap w-auto">{{ __('URL') }}</th>
-				<th class="text-nowrap w-auto">{{ __('Data examples') }}</th>
-			</tr>
-			</thead>
-			<tbody>
-				@foreach ($testSteps as $testStep)
-					<tr>
-						<td>
-							{{ __('Step :n', ['n' => $testStep->position]) }}
-						</td>
-						<td>
-							{{ $testStep->source->name }}
-						</td>
-						<td>
-							{{ $testStep->target->name }}
-						</td>
-						<td>
-							{{ $testStep->forward }}
-						</td>
-						<td>
-							@if ($testStep->testRequestExample)
-								<button type="button" class="btn btn-secondary border" data-fancybox data-src="#step-{{ $testStep->id }}-request">{{ __('Request') }}</button>
-								@include('sessions.test-steps.includes.request')
-							@endif
-							@if ($testStep->testResponseExample)
-								<button type="button" class="btn btn-secondary border" data-fancybox data-src="#step-{{ $testStep->id }}-response">{{ __('Response') }}</button>
-								@include('sessions.test-steps.includes.response')
-							@endif
-						</td>
-					</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</div>
-	@if ($testSteps->count())
-		<div class="card-footer">
-			<div class="row align-items-center">
-				<div class="col-md-6">
-					{{ __('Showing :from to :to of :total entries', [
-						'from' => (($testSteps->currentPage() - 1) * $testSteps->perPage()) + 1,
-						'to' => (($testSteps->currentPage() - 1) * $testSteps->perPage()) + $testSteps->count(),
-						'total' => $testSteps->total(),
-					]) }}
-				</div>
-				<div class="col-md-6">
-					<div class="justify-content-end d-flex">
-						{{ $testSteps->appends(request()->all())->links() }}
-					</div>
-				</div>
-			</div>
-		</div>
-	@endif
+    <div class="card">
+        <div class="table table-striped table-hover card-table">
+            <table class="table table-striped table-hover card-table">
+                <thead class="thead-light">
+                <tr>
+                    <th class="text-nowrap align-middle">{{ __('Step') }}</th>
+                    <th class="text-nowrap align-middle">{{ __('Source') }}</th>
+                    <th class="text-nowrap align-middle">{{ __('Target') }}</th>
+                    <th class="text-nowrap align-middle">{{ __('URL') }}</th>
+                    <th class="text-nowrap align-middle">{{ __('Data examples') }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach ($testSteps as $testStep)
+                        <tr>
+                            <td class="align-middle">
+                                {{ __('Step :n', ['n' => $testStep->position]) }}
+                            </td>
+                            <td class="align-middle">
+                                {{ $testStep->source->name }}
+                            </td>
+                            <td class="align-middle">
+                                {{ $testStep->target->name }}
+                            </td>
+                            <td class="align-middle">
+                                {{ $testStep->forward }}
+                            </td>
+                            <td class="align-middle">
+                                @if ($testStep->testRequestExample || $testStep->testResponseExample)
+                                    <div class="btn-group" role="group">
+                                        @if ($testStep->testRequestExample)
+                                            <button type="button" class="btn btn-secondary border" data-fancybox data-src="#step-{{ $testStep->id }}-request">{{ __('Request') }}</button>
+                                        @endif
+                                        @if ($testStep->testResponseExample)
+                                            <button type="button" class="btn btn-secondary border" data-fancybox data-src="#step-{{ $testStep->id }}-response">{{ __('Response') }}</button>
+                                        @endif
+                                    </div>
+                                @endif
+                                @if ($testStep->testRequestExample)
+                                    @include('sessions.test-steps.includes.request')
+                                @endif
+                                @if ($testStep->testResponseExample)
+                                    @include('sessions.test-steps.includes.response')
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+        @if ($testSteps->count())
+            <div class="card-footer">
+                <div class="row align-items-center">
+                    <div class="col-md-6">
+                        {{ __('Showing :from to :to of :total entries', [
+                            'from' => (($testSteps->currentPage() - 1) * $testSteps->perPage()) + 1,
+                            'to' => (($testSteps->currentPage() - 1) * $testSteps->perPage()) + $testSteps->count(),
+                            'total' => $testSteps->total(),
+                        ]) }}
+                    </div>
+                    <div class="col-md-6">
+                        <div class="justify-content-end d-flex">
+                            {{ $testSteps->appends(request()->all())->links() }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+    </div>
 @endsection
