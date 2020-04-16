f27985a6c83df53172ec7afe787daea5fe53f848
Alexander Kokorev <akokorev@justcoded.co>
2020-04-16 12:26:08 +0300

    Update env

--- src/app/Http/Controllers/Auth/VerificationController.php
+++ src/app/Http/Controllers/Auth/VerificationController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\VerifiesEmails;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class VerificationController extends Controller
 {
@@ -23,5 +25,16 @@ class VerificationController extends Controller
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Inertia\Response
+     */
+    public function show(Request $request)
+    {
+        return $request->user()->hasVerifiedEmail()
+            ? redirect($this->redirectPath())
+            : Inertia::render('auth/verify');
     }
 }
---
+++ src/resources/js/pages/auth/verify.vue
@@ -0,0 +1,49 @@
+<template>
+    <layout>
+        <form class="card card-md" @submit.prevent="submit">
+            <div class="card-body p-6">
+                <div class="card-title">
+                    Verify Your Email Address
+                </div>
+                <p class="text-muted">
+                    Before proceeding, please check your email for a verification link.
+                </p>
+                <div class="form-footer">
+                    <button type="submit" class="btn btn-primary btn-block">
+                        <span
+                            v-if="sending"
+                            class="spinner-border spinner-border-sm mr-2"
+                        ></span>
+                        Click here to request another
+                    </button>
+                </div>
+            </div>
+        </form>
+    </layout>
+</template>
+
+<script>
+import Layout from '@/layouts/auth';
+
+export default {
+    metaInfo: {
+        title: 'Verify Your Email Address'
+    },
+    components: {
+        Layout
+    },
+    data() {
+        return {
+            sending: false,
+        };
+    },
+    methods: {
+        submit() {
+            this.sending = true;
+            this.$inertia
+                .post(route('verification.resend'))
+                .then(() => (this.sending = false));
+        }
+    }
+};
+</script>
