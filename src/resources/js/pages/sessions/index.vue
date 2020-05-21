b61c670191e55eacde426caca18d4fe6e86b206a
jc_ohladkov <ohladkov@justcoded.co>
2020-05-21 19:02:12 +0300

    add empty boxes with messages

--- src/resources/js/pages/sessions/index.vue
+++ src/resources/js/pages/sessions/index.vue
@@ -10,100 +10,117 @@
             </div>
         </div>
         <div class="card">
-            <div class="card-header">
-                <form class="input-icon" @submit.prevent="search">
-                    <input
-                        v-model="form.q"
-                        type="text"
-                        class="form-control"
-                        placeholder="Search..."
-                    />
-                    <span class="input-icon-addon">
-                        <icon name="search" />
-                    </span>
-                </form>
+            <div class="empty h-auto" v-if="!sessions.data.length">
+                <div class="row">
+                    <div class="col-10 mx-auto">
+                        <p class="empty-title h3 mb-3">You have no sessions</p>
+                        <p class="empty-subtitle text-muted mb-0">
+                            Click the button below to create your first session.
+                        </p>
+                        <div class="empty-action">
+                            <inertia-link
+                                :href="route('sessions.register.sut')"
+                                class="btn btn-primary"
+                            >
+                                <icon name="plus" />
+                                New Session
+                            </inertia-link>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="table-responsive mb-0">
-                <table
-                    class="table table-striped table-vcenter table-hover card-table"
-                >
-                    <thead>
-                        <tr>
-                            <th class="text-nowrap w-25">Name</th>
-                            <th class="text-nowrap w-auto">Use Cases</th>
-                            <th class="text-nowrap w-auto">Test Cases</th>
-                            <th class="text-nowrap w-25">Status</th>
-                            <th class="text-nowrap w-auto">Last Run</th>
-                            <th class="text-nowrap w-1"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="session in sessions.data">
-                            <td class="text-break">
-                                <inertia-link
-                                    :href="route('sessions.show', session.id)"
-                                >
-                                    {{ session.name }}
-                                </inertia-link>
-                            </td>
-                            <td>
-                                {{ session.testCases ? collect(session.testCases).unique('use_case_id').count() : 0 }}
-                            </td>
-                            <td>
-                                {{ session.testCases ? session.testCases.length : 0  }}
-                            </td>
-                            <td>
-                                <session-progress :testCases="session.testCases" />
-                            </td>
-                            <td>
-                                {{ session.lastTestRun ? session.lastTestRun.created_at : '' }}
-                            </td>
-                            <td class="text-center text-break">
-                                <b-dropdown
-                                    v-if="session.can.delete"
-                                    no-caret
-                                    right
-                                    toggle-class="align-items-center text-muted"
-                                    variant="link"
-                                    boundary="window"
-                                >
-                                    <template v-slot:button-content>
-                                        <icon name="dots-vertical"></icon>
-                                    </template>
-                                    <li v-if="session.can.delete">
-                                        <confirm-link
-                                            class="dropdown-item"
-                                            :href="
-                                                route(
-                                                    'sessions.destroy',
-                                                    session.id
-                                                )
-                                            "
-                                            method="delete"
-                                            :confirm-title="'Confirm delete'"
-                                            :confirm-text="
-                                                `Are you sure you want to delete ${session.name}?`
-                                            "
-                                        >
-                                            Delete
-                                        </confirm-link>
-                                    </li>
-                                </b-dropdown>
-                            </td>
-                        </tr>
-                        <tr v-if="!sessions.data.length">
-                            <td class="text-center" colspan="6">
-                                No Results
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <pagination
-                :meta="sessions.meta"
-                :links="sessions.links"
-                class="card-footer"
-            />
+
+            <template v-else>
+                <div class="card-header">
+                    <form class="input-icon" @submit.prevent="search">
+                        <input
+                            v-model="form.q"
+                            type="text"
+                            class="form-control"
+                            placeholder="Search..."
+                        />
+                        <span class="input-icon-addon">
+                            <icon name="search" />
+                        </span>
+                    </form>
+                </div>
+                <div class="table-responsive mb-0">
+                    <table
+                        class="table table-striped table-vcenter table-hover card-table"
+                    >
+                        <thead>
+                            <tr>
+                                <th class="text-nowrap w-25">Name</th>
+                                <th class="text-nowrap w-auto">Use Cases</th>
+                                <th class="text-nowrap w-auto">Test Cases</th>
+                                <th class="text-nowrap w-25">Status</th>
+                                <th class="text-nowrap w-auto">Last Run</th>
+                                <th class="text-nowrap w-1"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="session in sessions.data">
+                                <td class="text-break">
+                                    <inertia-link
+                                        :href="route('sessions.show', session.id)"
+                                    >
+                                        {{ session.name }}
+                                    </inertia-link>
+                                </td>
+                                <td>
+                                    {{ session.testCases ? collect(session.testCases).unique('use_case_id').count() : 0 }}
+                                </td>
+                                <td>
+                                    {{ session.testCases ? session.testCases.length : 0  }}
+                                </td>
+                                <td>
+                                    <session-progress :testCases="session.testCases" />
+                                </td>
+                                <td>
+                                    {{ session.lastTestRun ? session.lastTestRun.created_at : '' }}
+                                </td>
+                                <td class="text-center text-break">
+                                    <b-dropdown
+                                        v-if="session.can.delete"
+                                        no-caret
+                                        right
+                                        toggle-class="align-items-center text-muted"
+                                        variant="link"
+                                        boundary="window"
+                                    >
+                                        <template v-slot:button-content>
+                                            <icon name="dots-vertical"></icon>
+                                        </template>
+                                        <li v-if="session.can.delete">
+                                            <confirm-link
+                                                class="dropdown-item"
+                                                :href="
+                                                    route(
+                                                        'sessions.destroy',
+                                                        session.id
+                                                    )
+                                                "
+                                                method="delete"
+                                                :confirm-title="'Confirm delete'"
+                                                :confirm-text="
+                                                    `Are you sure you want to delete ${session.name}?`
+                                                "
+                                            >
+                                                Delete
+                                            </confirm-link>
+                                        </li>
+                                    </b-dropdown>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <pagination
+                    :meta="sessions.meta"
+                    :links="sessions.links"
+                    class="card-footer"
+                />
+            </template>
         </div>
     </layout>
 </template>
