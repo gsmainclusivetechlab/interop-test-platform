ed4587da06c0628a9df380caf6b8119bac3c9dbc
Alexander Kokorev <akokorev@justcoded.co>
2020-07-30 13:28:53 +0300

    Adding ability to see group sessions in home/sessions pages

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\SessionResource;
+use App\Models\Session;
+use Illuminate\Database\Eloquent\Builder;
 use Inertia\Inertia;
 
 class HomeController extends Controller
@@ -22,9 +24,14 @@ class HomeController extends Controller
     {
         return Inertia::render('home', [
             'sessions' => SessionResource::collection(
-                auth()
-                    ->user()
-                    ->sessions()
+                Session::whereHas('owner', function (Builder $query) {
+                    $query->whereKey(auth()->user())
+                        ->orWhereHas('groups', function (Builder $query) {
+                            $query->whereHas('members', function (Builder $query) {
+                                $query->whereKey(auth()->user());
+                            });
+                        });
+                })
                     ->with([
                         'testCases' => function ($query) {
                             return $query->with(['useCase', 'lastTestRun']);
