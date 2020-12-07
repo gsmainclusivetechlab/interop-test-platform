60c960d46217a8c8926c469d9a107faca3a09429
Kolya Siryk <kolyasirik@gmail.com>
2020-12-07 13:19:28 +0200

    Control Available Session Modes

--- src/config/service_session.php
+++ src/config/service_session.php
@@ -4,14 +4,9 @@ return [
     'compliance_session_execution_limit' =>
         env('COMPLIANCE_SESSION_EXECUTION_LIMIT') ?: 5,
 
-//    'available_modes' => [
-//        'test' => env('SESSION_TEST_AVAILABLE', true),
-//        'test_questionnaire' => env('SESSION_TEST_QUESTIONNAIRE_AVAILABLE', true),
-//        'compliance' => env('SESSION_COMPLIANCE_AVAILABLE', true)
-//    ]
     'available_modes' => [
-        'test' => false,
-        'test_questionnaire' => false,
-        'compliance' => false
+        'test' => env('SESSION_TEST_AVAILABLE', true),
+        'test_questionnaire' => env('SESSION_TEST_QUESTIONNAIRE_AVAILABLE', true),
+        'compliance' => env('SESSION_COMPLIANCE_AVAILABLE', true)
     ]
 ];
