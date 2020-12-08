7e26dcc329cc892ee37ffad4451c3ce157cab33e
dsymonov <dsymonov@justcoded.co>
2020-12-08 12:53:01 +0200

    update json editor

--- src/resources/js/components/json-editor-block.vue
+++ src/resources/js/components/json-editor-block.vue
@@ -1,29 +1,48 @@
 <template>
-    <b-tabs content-class="json-editor-block mt-2">
-        <b-tab title="Editor" title-link-class="rounded-0">
+    <div class="json-editor-block row mt-2">
+        <div class="col-6">
             <json-editor
-                :options="{
-                    confirmText: 'Confirm',
-                    cancelText: 'Cancel',
-                }"
-                :objData="jsonData"
-                v-model="jsonData"
-            ></json-editor>
-        </b-tab>
-        <b-tab title="Preview" title-link-class="rounded-0">
+                class="json-editor"
+                :data-input="jsonData"
+                @data-output="(data) => (jsonData = data)"
+            >
+                <template #icon-add>
+                    <icon name="plus" v-b-tooltip.hover title="Add" />
+                </template>
+                <template #icon-delete>
+                    <icon name="trash" v-b-tooltip.hover title="Delete" />
+                </template>
+                <template #icon-drag>
+                    <icon name="arrows-sort" v-b-tooltip.hover title="Move" />
+                </template>
+                <template #icon-collapse>
+                    <icon
+                        name="chevron-up"
+                        v-b-tooltip.hover
+                        title="Collapse"
+                    />
+                </template>
+            </json-editor>
+        </div>
+        <div class="col-6">
             <json-tree
                 :data="jsonData"
                 :deep="2"
                 :show-line="false"
                 class="p-2"
             ></json-tree>
-        </b-tab>
-    </b-tabs>
+        </div>
+    </div>
 </template>
 
 <script>
+import JsonEditor from '@kassaila/vue-json-editor';
+
 export default {
     name: 'JsonEditorBlock',
+    components: {
+        JsonEditor,
+    },
     props: {
         inputJson: {
             type: Object,
