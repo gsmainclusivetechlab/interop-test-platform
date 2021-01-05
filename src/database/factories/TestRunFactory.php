3b75978354c38b47997649f62c9c5e3576a349c6
Samy <sbouchlaghem@gsma.com>
2021-01-05 19:16:53 +0000

    test execution, result and run

---
+++ src/database/factories/TestRunFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\TestCase;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(TestRun::class, function (Faker $faker) {
+    return [
+        'test_case_id' => function () {
+            return factory(TestCase::class)
+                ->create()
+                ->getKey();
+        },
+        'completed_at' => $faker->datetime,
+    ];
+});
