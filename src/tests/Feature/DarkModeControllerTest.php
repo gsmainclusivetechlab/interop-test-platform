607acbb89e0b5c3d8df7acc09e578aaaacd60e38
Samy <sbouchlaghem@gsma.com>
2021-01-21 08:48:30 +0000

    registercontrollertest

---
+++ src/tests/Feature/DarkModeControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Controllers\TutorialController;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Cookie;
+use Tests\TestCase;
+
+class DarkModeControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testDarkModeController()
+    {
+
+        $response = $this->post('/dark-mode');
+        $response->assertCookie('dark_mode', true);
+        $response->assertStatus(302);
+    }
+}
