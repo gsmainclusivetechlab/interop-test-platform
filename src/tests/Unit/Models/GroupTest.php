c762daf5a3742e043d1706e05683324221d27eed
SamyBouch <sbouchlaghem@gsma.com>
2021-02-04 22:29:15 +0000

    Unit test coverage (#414)
    * Add tests for Group, GroupEnvironment and GroupUserInvitation
    
    * test execution, result and run
    
    * (wip) test run, result
    
    * registercontrollertest
    
    * Reorder tests, additional controller tests
    
    * compliancesessioncontrollertest + small changes to registercontroller,messagemismatch
    
    * Add tests for Group, GroupEnvironment and GroupUserInvitation
    
    * test execution, result and run
    
    * (wip) test run, result
    
    * registercontrollertest
    
    * Reorder tests, additional controller tests
    
    * compliancesessioncontrollertest + small changes to registercontroller,messagemismatch
    
    * group controller tests
    
    * tests update
    
    * admin group, use case, user tests

---
+++ src/tests/Unit/Models/GroupTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Group;
+use Tests\TestCase;
+
+class GroupTest extends TestCase
+{
+    /**
+     * Test Group store.
+     *
+     * @return void
+     */
+    public function testGroupStore()
+    {
+        $group = factory(Group::class)->make();
+        $this->assertValidationPasses($group->getAttributes(), [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['string'],
+            'domain' => ['string'],
+        ]);
+        $this->assertTrue($group->save());
+    }
+
+    /**
+     * Test Group delete.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function testGroupDelete()
+    {
+        $group = factory(Group::class)->create();
+        $group->delete();
+        $this->assertDeleted($group);
+    }
+}
