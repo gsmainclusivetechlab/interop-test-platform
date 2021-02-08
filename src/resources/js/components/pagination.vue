08e5de48fe229fedbe76ad2e55a1a311dcb10e57
Dmytro Symonov <75664772+dsymonov-jc@users.noreply.github.com>
2021-02-08 12:06:22 +0200

    Feature/pagination fix (#437)
    * pagination - fix 
    
    * add navigation input
    
    * add badge current page
    
    * add validation for page set

--- src/resources/js/components/pagination.vue
+++ src/resources/js/components/pagination.vue
@@ -16,7 +16,7 @@
             {{ $tc('comment[3]', meta.total) }}
         </p>
         <ul v-if="meta.total > meta.per_page" class="pagination m-0 ml-auto">
-            <li v-if="links.prev" class="page-item">
+            <li v-if="links.prev !== null" class="page-item">
                 <inertia-link class="page-link" :href="links.prev">
                     <icon name="arrow-left"></icon>
                     {{ $t('buttons.prev') }}
@@ -28,21 +28,69 @@
                     {{ $t('buttons.prev') }}
                 </span>
             </li>
-            <li
-                v-for="(n, i) in meta.last_page"
-                class="page-item"
-                :class="{ active: meta.current_page === n }"
-                :key="i"
-            >
-                <inertia-link
-                    class="page-link"
-                    :href="route().url()"
-                    :data="{ page: n }"
+            <template v-for="(n, i) in meta.last_page">
+                <li
+                    v-if="
+                        meta.last_page <= navigationLimit.pages ||
+                        listPageBySides.some((val) => val === n)
+                    "
+                    class="page-item"
+                    :class="{ active: meta.current_page === n }"
+                    :key="i"
                 >
-                    {{ n }}
-                </inertia-link>
-            </li>
-            <li v-if="links.next" class="page-item">
+                    <inertia-link
+                        class="page-link"
+                        :href="route().url()"
+                        :data="{ page: n }"
+                    >
+                        {{ n }}
+                    </inertia-link>
+                </li>
+                <li
+                    v-else-if="
+                        [
+                            meta.last_page - (navigationLimit.pageBySides + 2),
+                            meta.last_page - navigationLimit.pageBySides,
+                        ].some((val) => val === n)
+                    "
+                    class="page-item"
+                    :key="i"
+                >
+                    <span class="page-link">...</span>
+                </li>
+                <li
+                    v-else-if="
+                        meta.last_page - (navigationLimit.pageBySides + 1) === n
+                    "
+                    class="pagination__navigation"
+                    :key="i"
+                >
+                    <form class="input-group" @submit.prevent="navigateToPage">
+                        <input
+                            v-model="navigation.pageTo"
+                            type="number"
+                            class="form-control"
+                            :class="{
+                                'is-invalid': !checkPageTo,
+                            }"
+                        />
+                        <button type="submit" class="btn btn-outline-primary">
+                            Go
+                        </button>
+                    </form>
+                    <span
+                        v-if="
+                            listPageBySides.every(
+                                (val) => val !== meta.current_page
+                            )
+                        "
+                        class="badge"
+                        v-b-tooltip.hover.top="'Current page'"
+                        >{{ meta.current_page }}</span
+                    >
+                </li>
+            </template>
+            <li v-if="links.next !== null" class="page-item">
                 <inertia-link class="page-link" :href="links.next">
                     {{ $t('buttons.next') }}
                     <icon name="arrow-right"></icon>
@@ -68,6 +116,52 @@ export default {
         links: {
             type: Object,
             required: true,
+        },
+        navigationLimit: {
+            type: Object,
+            required: false,
+            default() {
+                return {
+                    pages: 10,
+                    pageBySides: 2,
+                };
+            },
+        },
+    },
+    data() {
+        return {
+            navigation: {
+                pageTo: this.meta.current_page,
+            },
+        };
+    },
+    methods: {
+        navigateToPage() {
+            if (!this.checkPageTo) return;
+
+            this.$inertia.visit(route().url(), {
+                method: 'get',
+                data: {
+                    page: this.navigation.pageTo,
+                },
+            });
+        },
+    },
+    computed: {
+        listPageBySides() {
+            const arr = [...Array(this.navigationLimit.pageBySides).keys()];
+            const arrBefore = arr.map((val) => val + 1);
+            const arrAfter = arr
+                .reverse()
+                .map((val) => this.meta.last_page - val);
+
+            return [...arrBefore, ...arrAfter];
+        },
+        checkPageTo() {
+            return (
+                this.navigation.pageTo > 0 &&
+                this.navigation.pageTo <= this.meta.last_page
+            );
         },
     },
 };
