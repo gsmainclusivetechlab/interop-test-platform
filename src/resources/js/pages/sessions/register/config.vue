0960c51ff8bd88117bbafeb327e2ddcbc8355b02
Christopher Little <clittle@gsma.com>
2021-02-17 18:07:52 +0000

    Allow external configuration of testing URLs

--- src/resources/js/pages/sessions/register/config.vue
+++ src/resources/js/pages/sessions/register/config.vue
@@ -252,11 +252,19 @@ export default {
     },
     mixins: [mixinVSelect],
     data() {
-        const ziggyConf = { ...this.route().ziggy };
-        ziggyConf.baseUrl = this.$page.props.app.http_base_url;
+        const ziggyConf = this.route().ziggy;
 
         return {
-            ziggyConf,
+            ziggyConf: {
+                http: {
+                    ...ziggyConf,
+                    baseUrl: this.$page.props.app.testing_url_http,
+                },
+                https: {
+                    ...ziggyConf,
+                    baseUrl: this.$page.props.app.testing_url_https,
+                },
+            },
             sending: false,
             groupEnvironment: null,
             groupEnvironmentsList: [],
@@ -318,16 +326,19 @@ export default {
         getRoute(useEncryption, data, isForGroup = false) {
             const group = isForGroup ? '-group' : '';
 
-            if (useEncryption) {
-                return this.route('testing' + group + '.sut', data);
-            }
-
-            return this.route(
-                'testing-insecure' + group + '.sut',
-                data,
-                undefined,
-                this.ziggyConf
-            );
+            return useEncryption
+                ? this.route(
+                      `testing${group}.sut`,
+                      data,
+                      undefined,
+                      this.ziggyConf.https
+                  )
+                : this.route(
+                      `testing-insecure${group}.sut`,
+                      data,
+                      undefined,
+                      this.ziggyConf.http
+                  );
         },
     },
 };
