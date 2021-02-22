190c08808b2cfa48912a8f7bc9224924ba279220
jc-skushnir <53338807+jc-skushnir@users.noreply.github.com>
2021-02-22 17:36:05 +0200

    Feature/406 beautify yaml export (#470)
    * update test case export, add count inline param for yaml dump
    
    Co-authored-by: Serhii Kushnir <skushnir@justcoded.com>

--- src/app/Exports/TestCaseExport.php
+++ src/app/Exports/TestCaseExport.php
@@ -19,11 +19,28 @@ class TestCaseExport implements Exportable
         $data = $this->getArray($testCase);
 
         return Yaml::dump(
-            $this->arrayFilter($data),
-            2,
+            $data,
+            $this->getInline($data),
             2,
             Yaml::DUMP_MULTI_LINE_LITERAL_BLOCK
         );
+    }
+
+    /**
+     * @param array $array
+     * @return int
+     */
+    protected function getInline(array $array): int
+    {
+        $depth = [0];
+        foreach ($array as $item)
+        {
+            if (is_array($item)) {
+                $depth[] = $this->getInline($item);
+            }
+        }
+
+        return 1 + max($depth);
     }
 
     /**
@@ -33,7 +50,9 @@ class TestCaseExport implements Exportable
      */
     public function getArray($testCase): array
     {
-        return $this->mapTestCase($testCase);
+        return $this->arrayFilter(
+            $this->mapTestCase($testCase)
+        );
     }
 
     /**
@@ -85,7 +104,7 @@ class TestCaseExport implements Exportable
                 'method' => $testStep->method,
                 'source' => $testStep->source->slug,
                 'target' => $testStep->target->slug,
-                'mtls' => $testStep->mtls,
+                'mtls' => (bool) $testStep->mtls ?: null,
                 'api_spec' => $testStep->apiSpec()->exists()
                     ? $testStep->apiSpec->name
                     : null,
@@ -140,8 +159,13 @@ class TestCaseExport implements Exportable
                 TestScript::TYPE_REPEAT_RESPONSE
             ),
         ];
+        $result = $this->arrayFilter($result);
+        if (2 == count($result))
+        {
+            return null;
+        }
 
-        return $this->arrayFilter($result);
+        return $result;
     }
 
     /**
