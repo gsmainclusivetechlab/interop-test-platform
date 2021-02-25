0c654704da33eb9447a3191726151458627965bf
Kolya Siryk <kolyasirik@gmail.com>
2021-02-25 12:56:14 +0200

    fix jws

---
+++ src/database/migrations/2021_02_25_104815_update_test_result_request_and_response.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTestResultRequestAndResponse extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('test_results', function (Blueprint $table) {
+            $table
+                ->text('request')
+                ->nullable()
+                ->change();
+            $table
+                ->text('response')
+                ->nullable()
+                ->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('test_results', function (Blueprint $table) {
+            $table
+                ->json('request')
+                ->nullable()
+                ->change();
+            $table
+                ->json('response')
+                ->nullable()
+                ->change();
+        });
+    }
+}
