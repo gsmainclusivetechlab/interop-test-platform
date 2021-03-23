7b5510ee51f4c45ead61e86eb34a6865414684e0
SamyBouch <sbouchlaghem@gsma.com>
2021-03-23 12:31:31 +0000

    Overwrite empty envs merge group envs (#503)
    * improve merge envs to overwrite empty fields
    
    * update group env merge logic
    
    * Copy logic to file envs

--- src/resources/js/pages/sessions/mixins/environments.js
+++ src/resources/js/pages/sessions/mixins/environments.js
@@ -3,6 +3,16 @@ export default {
         mergeEnvs(incomingEnvs, currentEnvs, envsType) {
             switch (envsType) {
                 case 'text': {
+                    Object.keys(currentEnvs).forEach(function (key) {
+                        if (
+                            (currentEnvs[key].value === null ||
+                                currentEnvs[key].value === '') &&
+                            incomingEnvs[currentEnvs[key].key]
+                        ) {
+                            currentEnvs[key].value =
+                                incomingEnvs[currentEnvs[key].key];
+                        }
+                    });
                     Object.entries(incomingEnvs)
                         .filter(
                             ([key, value]) =>
@@ -17,6 +27,17 @@ export default {
                     break;
                 }
                 case 'file': {
+                    currentEnvs.forEach(function (current) {
+                        if (!current.file_name || !current.value) {
+                            const incoming = incomingEnvs.find(
+                                (incoming) => incoming.name === current.key
+                            );
+                            if (incoming) {
+                                current.value = incoming.id;
+                                current.file_name = incoming.file_name;
+                            }
+                        }
+                    });
                     incomingEnvs
                         .filter(
                             (incoming) =>
