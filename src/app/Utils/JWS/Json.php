7c9b7f453123c61a538619b67dab4815a3392f1c
Serhii Kushnir <skushnir@justcoded.com>
2021-04-06 00:08:21 +0300

    fix jws generation with special characters

--- src/app/Utils/JWS/Json.php
+++ src/app/Utils/JWS/Json.php
@@ -6,7 +6,7 @@ class Json extends \Gamegos\JWS\Util\Json
 {
     public static function encode($data)
     {
-        $json = @json_encode($data, JSON_UNESCAPED_SLASHES);
+        $json = @json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
         if (JSON_ERROR_NONE !== json_last_error()) {
             throw new \InvalidArgumentException(self::transformJsonError());
