58cec1cf5e0d929ad1088df0b6cad16052aa3c3a
Samy <sbouchlaghem@gsma.com>
2021-06-24 10:34:44 +0100

    add tab name to locale file

--- src/resources/js/layouts/app.vue
+++ src/resources/js/layouts/app.vue
@@ -49,8 +49,8 @@ export default {
                     : [],
             titleTemplate: (title) =>
                 title
-                    ? `${title} - Interoperability Test Platform - GSMA`
-                    : 'Interoperability Test Platform - GSMA',
+                    ? `${title} - ` + this.$t('layout.main.main-nav.tab-name')
+                    : this.$t('layout.main.main-nav.tab-name'),
         };
     },
     mounted() {
