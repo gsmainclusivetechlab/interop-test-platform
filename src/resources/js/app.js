7dbc945aebd0be1e9ee5c2a2752e000b94e0fe45
Yaroslav <yvoitenko@justcoded.com>
2022-03-20 00:00:54 +0200

    restored previous inertia version

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import VueMeta from 'vue-meta';
-import { App, plugin, Link } from '@inertiajs/inertia-vue';
+import { App, plugin } from '@inertiajs/inertia-vue';
 import {
     NavPlugin,
     NavbarPlugin,
@@ -52,8 +52,6 @@ Vue.mixin({
 });
 
 Vue.config.productionTip = false;
-
-Vue.component('inertia-link', Link);
 
 Vue.component('icon', () =>
     import(/* webpackChunkName: "icon" */ '@/components/icon.vue')
